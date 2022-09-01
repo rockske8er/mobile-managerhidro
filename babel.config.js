cf8cd919bfb1dd74d4d22724e32671f40ac77368
rockske8er <rockske8er@gmail.com>
2022-09-01 17:04:02 -0300

    Create project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'], 
+    plugins: [
+      [
+        'module-resolver', 
+        {
+          root: './src',
+          alias: {
+            '@Assets': './src/assets',
+            '@Components': './src/components',
+            '@Routes': './src/routes',
+            '@Screens': './src/screens',
+            '@Services': './src/services',
+            '@Themes': './src/themes',
+          }
+        }
+      ]
+    ]
   };
 };
